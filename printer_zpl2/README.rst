902bf92d4a8907e63b47ed03ec18e15cea9bf2bc
Florent de Labarre <florent.mirieu@gmail.com>
2018-01-20 23:20:11 +0100

    [IMP] Add wizard to import ZPL2

--- printer_zpl2/README.rst
+++ printer_zpl2/README.rst
@@ -24,6 +24,7 @@ To configure this module, you need to:
 
 #. Go to *Settings > Printing > Labels > ZPL II*
 #. Create new labels
+#. Import ZPL2 code
 #. Use the Test Mode tab during the creation
 
 It's also possible to add a label printing wizard on any model by creating a new *ir.actions.act_window* record.
--- printer_zpl2/models/printing_label_zpl2.py
+++ printer_zpl2/models/printing_label_zpl2.py
@@ -263,6 +263,18 @@ class PrintingLabelZpl2(models.Model):
     def unlink_action(self):
         self.mapped('action_window_id').unlink()
 
+    @api.multi
+    def import_zpl2(self):
+        self.ensure_one()
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'wizard.import.zpl2',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': {'default_label_id': self.id},
+        }
+
     def _get_record(self):
         self.ensure_one()
         Obj = self.env[self.model_id.model]
